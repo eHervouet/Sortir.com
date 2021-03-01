4ed86d660dbd4674c817e77ff59f7d8e1be413b0
Vydr'Oz <61025448+VydrOz@users.noreply.github.com>
2021-03-01 15:30:20 +0100

    add form register/login

---
+++ src/Form/RegisterType.php
@@ -0,0 +1,55 @@
+<?php
+
+namespace App\Form;
+
+use App\Entity\User;
+use Symfony\Component\Form\AbstractType;
+use Symfony\Component\Form\FormBuilderInterface;
+use Symfony\Component\OptionsResolver\OptionsResolver;
+use Symfony\Component\Form\Extension\Core\Type\EmailType;
+use Symfony\Component\Form\Extension\Core\Type\RepeatedType;
+use Symfony\Component\Form\Extension\Core\Type\PasswordType;
+use Symfony\Component\Form\Extension\Core\Type\SubmitType;
+
+class RegisterType extends AbstractType
+{
+    public function buildForm(FormBuilderInterface $builder, array $options)
+    {
+        $builder
+        ->add('username', null, [
+            'label' => 'User Name',
+            'attr' =>
+                ['placeholder' => 'Your user name']
+        ])
+        ->add('password', RepeatedType::class, [
+            'type' => PasswordType::class,
+            'invalid_message' => 'The password fields must match.',
+            'required' => true,
+            'first_options'  => [
+                'label' => 'Password',
+                'attr' =>
+                    ['placeholder' => '********']
+            ],
+            'second_options' => [
+                'label' => 'Repeat Password',
+                'attr' =>
+                    ['placeholder' => '********']
+            ],
+        ])
+        ->add('e_mail', EmailType::class, [
+            'label' => 'E-Mail',
+            'attr' =>
+                ['placeholder' => 'exemple@this.com']
+        ])
+        ->add('add', SubmitType::class, [
+            'label' => 'Register'
+        ]);
+    }
+
+    public function configureOptions(OptionsResolver $resolver)
+    {
+        $resolver->setDefaults([
+            'data_class' => User::class,
+        ]);
+    }
+}
